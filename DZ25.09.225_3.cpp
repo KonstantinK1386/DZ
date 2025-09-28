cd86aa95d3d92b65bd07aadd0466f03e87406d41
Konstantin <Glefa86@mail.ru>
2025-09-28 12:32:31 +0300

    Add files via upload

--- DZ25.09.225_3.cpp
+++ DZ25.09.225_3.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <Windows.h>
 
-void numb_fibonachchi(int num, unsigned long int a, unsigned long int b) { // начало функции перечисления чисел Фибоначчи
-	unsigned long int temp; // формат unsigned long int - только целые длинное целое число позволяет хранитт 80 число Фибоначчи
+void numb_fibonachchi(unsigned int num, unsigned long long int a, unsigned long long int b) { // начало функции перечисления чисел Фибоначчи
+	unsigned long long int temp; // формат unsigned long int - только целые длинное целое число позволяет хранитт 80 число Фибоначчи
 
 	temp = a;
 	a = b;
@@ -21,12 +21,25 @@ int main() {
 	SetConsoleCP(1251);
 	setlocale(LC_ALL, "RU");
 
-	int num;
+	unsigned int num;
 	std::cout << "Введите число : ";
 	std::cin >> num; // ввод колличество выводимых чисел Фибоначчи
-	std::cout << "Числа Фибоначи : 0, 1, ";
-	numb_fibonachchi(num - 2, 0, 1); // вызов функции вывода чисел Фибоначчи
-	std::cout << '\n';
-
+	if (num > 0) { // проверка ввода положительного числа
+		if (num > 2) { // если число не первые 2
+			std::cout << "Числа Фибоначи : 0, 1, ";
+			numb_fibonachchi(num - 2, 0, 1); // вызов функции вывода чисел Фибоначчи
+			std::cout << '\n';
+		}
+		else {
+			switch (num) { // если число равно 1 или 2
+			case 1: std::cout << "Числа Фибоначи : 0 \n ";
+				break;
+			case 2: std::cout << "Числа Фибоначи : 0, 1 \n ";
+				break;
+			}
+		};
+	}
+	else std::cout << "Вы не ввели колличество чилел или оно равно 0 \n";
+	
 	return 0;
 }
