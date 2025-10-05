7814fbc01450488b6b8a88ea1efe8082f8a8c1ea
Konstantin <Glefa86@mail.ru>
2025-10-05 17:40:17 +0300

    Add files via upload

---
+++ DZ02.10.2025_1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+void counting_function() {
+    static int counter = 0;
+    counter++;
+    std::cout << "Количество вызовов функции counting_function(): " << counter << '\n';
+}
+    
+void main(){
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    setlocale(LC_ALL, "RU");
+
+    for (int i = 0; i < 15; i++)
+    {
+        counting_function();
+    }
+}
---
+++ DZ02.10.2025_2.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+namespace sum {
+	int calc(int x, int y) {
+		return x + y;
+	}
+}
+namespace subtrac {
+	int calc(int x, int y) {
+		return x - y;
+	}
+}
+namespace multiplic {
+	int calc(int x, int y) {
+		return x * y;
+	}
+}
+namespace division {
+	int calc(int x, int y) {
+		return x / y;
+	}
+}
+
+void main() {
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	setlocale(LC_ALL, "RU");
+
+	int x = 6, y = 9;
+	std::cout << "x = " << x << " y = " << y << '\n';
+	std::cout << "Сложение : " << sum::calc(x, y) << '\n';
+	std::cout << "Вычитание : " << subtrac::calc(x, y) << '\n';
+	std::cout << "Умножение : " << multiplic::calc(x, y) << '\n';
+	std::cout << "Деление : " << division::calc(x, y) << '\n';
+
+
+
+}
+
+
+
