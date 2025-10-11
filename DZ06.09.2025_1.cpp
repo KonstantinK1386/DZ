ca967d7c6538c09d109068ec358197396be00764
Konstantin <Glefa86@mail.ru>
2025-10-11 17:11:55 +0300

    Add files via upload

---
+++ DZ06.09.2025_1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+void print(int* arr, int size) {
+	for (int i = 0; i < size; i++) {
+		std::cout << arr[i] << ' ';
+	}
+	std::cout << '\n';
+		
+}
+
+void main() {
+
+	int arr_1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int arr_2[] = {6, 5, 4, 8};
+	int arr_3[] = {1, 4, 3, 7, 5};
+
+	print(arr_1, (sizeof(arr_1) / sizeof(arr_1[0])));
+	print(arr_2, (sizeof(arr_2) / sizeof(arr_2[0])));
+	print(arr_3, (sizeof(arr_3) / sizeof(arr_3[0])));
+}
---
+++ DZ06.09.2025_2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+void swap(int* a, int* b) {
+
+	*a = *a + *b;
+	*b = *a - *b;
+	*a = *a - *b;
+}
+
+void main() {
+	int a = 5, b = 8;
+	int* p_a = &a;
+	int* p_b = &b;
+
+	std::cout << "a = " << *p_a << ", b = " << *p_b << std::endl;
+
+	swap(p_a, p_b);
+
+	std::cout << "a = " << *p_a << ", b = " << *p_b << std::endl;
+
+}
