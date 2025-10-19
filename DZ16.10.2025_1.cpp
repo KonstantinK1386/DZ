413ba509c8de88bc32f8c82e6951f68244422cb8
Konstantin <Glefa86@mail.ru>
2025-10-20 02:35:32 +0300

    Add files via upload

---
+++ DZ16.10.2025_1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <cstring>
+
+void main() {
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	setlocale(LC_ALL, "RU");
+
+	std::string Name = "";
+	std::string Surname = "";
+	std::cout << "Введите имя : ";
+	std::cin >> Name;
+	std::cout << "Введите фамилию : ";
+	std::cin >> Surname;
+	std::cout << "Здравствуйте " << (Name + " " + Surname) << '\n';
+
+}
---
+++ DZ16.10.2025_2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <cstring>
+
+void main() {
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	setlocale(LC_ALL, "RU");
+
+	std::string Word = "малина";
+	std::string str = "";
+	std::cout << "Угадайте слово : ";
+	std::cin >> str;
+	while ((Word == str) != 1) {
+		std::cout << "Неправильно \n";
+		std::cout << "Угадайте слово : ";
+		std::cin >> str;
+	}
+	std::cout << "Правильно! Вы победили! загадонное слово - " << Word << '\n';
+}
