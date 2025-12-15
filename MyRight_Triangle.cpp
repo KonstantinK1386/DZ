7bbd3b6e180da3e276cc0a65906b2ff1ff7f8586
Konstantin <Glefa86@mail.ru>
2025-12-15 20:24:21 +0300

    Delete MyRight_Triangle.cpp

--- MyRight_Triangle.cpp
+++
@@ -1,8 +0,0 @@
-﻿#include <iostream>
-#include <string>
-#include <Windows.h>
-#include "MyRight_Triangle.h"
-
-Right_Triangle::Right_Triangle(int side_a, int side_b, int side_c, int angle_A, int angle_B) : Triangle(side_a, side_b, side_c, angle_A, angle_B, 90) {// вызов конструктора треугольник
-		Name = "Прямоугольный треугольник ";
-	}
