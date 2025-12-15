f35f11c06e8565c47e0d0b29d077f69c0848e483
Konstantin <Glefa86@mail.ru>
2025-12-15 20:24:12 +0300

    Delete MyRhombus.h

--- MyRhombus.h
+++
@@ -1,7 +0,0 @@
-﻿#pragma once
-#include "MyParallelogramm.h"
-
-class  Rhombus : public Parallelogramm {// Ромб
-public:
-	Rhombus(int side_a, int angle_A, int angle_B); // Вызов конструктора ромб
-};
