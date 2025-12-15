a9511b9bb434771d2652cb529415615b5a569fb6
Konstantin <Glefa86@mail.ru>
2025-12-15 20:22:53 +0300

    Delete MyParallelogramm.cpp

--- MyParallelogramm.cpp
+++
@@ -1,5 +0,0 @@
-﻿#include "MyParallelogramm.h"
-
-Parallelogramm::Parallelogramm(int side_a, int side_b, int angle_A, int angle_B) : Qadrangle(side_a, side_b, side_a, side_b, angle_A, angle_B, angle_A, angle_B) { // вызов конструктора четырёхугольник
-		Name = "Параллелограмм ";
-	}
