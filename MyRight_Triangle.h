2960329bbe93a0ca390eb400a1aeea433220c43a
Konstantin <Glefa86@mail.ru>
2025-12-15 20:24:29 +0300

    Delete MyRight_Triangle.h

--- MyRight_Triangle.h
+++
@@ -1,8 +0,0 @@
-﻿#pragma once
-#include <iostream>
-#include "MyTriangle.h"
-
-class Right_Triangle : public Triangle {// прямоугольный треугольник
-public:
-	Right_Triangle(int side_a, int side_b, int side_c, int angle_A, int angle_B); // вызов конструктора треугольник
-};
