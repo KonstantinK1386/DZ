626864219a17ef9451542804a911c84dd62561a0
Konstantin <Glefa86@mail.ru>
2025-12-15 20:31:11 +0300

    Add files via upload

---
+++ DZ_Class/MyClass.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include "MyClass.h"
+
+	Counter::Counter() { count = 1; };
+	Counter::Counter(int count) {
+		this->count = count;
+	};
+	void Counter::print_count() {
+		std::cout << count << '\n';
+	};
+	void Counter::increase_the_counter() { count++; };
+	void Counter::lower_the_counter() { count--; };
