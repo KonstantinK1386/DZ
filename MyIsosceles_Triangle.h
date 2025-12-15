f7b984d5707072c02e1e53af44eb8376937525eb
Konstantin <Glefa86@mail.ru>
2025-12-15 20:22:38 +0300

    Delete MyIsosceles_Triangle.h

--- MyIsosceles_Triangle.h
+++
@@ -1,7 +0,0 @@
-﻿#pragma once
-#include "MyTriangle.h"
-
-class Isosceles_Triangle : public Triangle {// равнобедренный треугольник
-public:
-	Isosceles_Triangle(int side_a, int side_b, int angle_A, int angle_B);// вызов конструктора треугольник
-};
