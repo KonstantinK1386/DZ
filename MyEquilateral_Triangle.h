317cf214c584b233aed3e3b853edfc3473b9a3a7
Konstantin <Glefa86@mail.ru>
2025-12-15 20:21:57 +0300

    Delete MyEquilateral_Triangle.h

--- MyEquilateral_Triangle.h
+++
@@ -1,7 +0,0 @@
-﻿#pragma once
-#include "MyTriangle.h"
-
-class Equilateral_Triangle : public Triangle {// равносторонний треугольник
-public:
-	Equilateral_Triangle(int side_a);// вызов конструктора равностороннего треугольника
-};
