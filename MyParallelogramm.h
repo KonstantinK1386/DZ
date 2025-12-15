894de55e6512cef385a11edfa6f3dffbaa6d0f26
Konstantin <Glefa86@mail.ru>
2025-12-15 20:23:02 +0300

    Delete MyParallelogramm.h

--- MyParallelogramm.h
+++
@@ -1,7 +0,0 @@
-﻿#pragma once
-#include "MyQadrangle.h"
-
-class Parallelogramm : public Qadrangle {//параллелограмм
-public:
-	Parallelogramm(int side_a, int side_b, int angle_A, int angle_B); // вызов конструктора четырёхугольник
-};
