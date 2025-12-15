ccee57c749533f1227debcf2f5e6f11e05566a4e
Konstantin <Glefa86@mail.ru>
2025-12-15 20:22:28 +0300

    Delete MyIsosceles_Triangle.cpp

--- MyIsosceles_Triangle.cpp
+++
@@ -1,5 +0,0 @@
-﻿#include "MyIsosceles_Triangle.h"
-
-Isosceles_Triangle::Isosceles_Triangle(int side_a, int side_b, int angle_A, int angle_B) : Triangle(side_a, side_b, side_a, angle_A, angle_B, angle_A) {// вызов конструктора треугольник
-		Name = "Равнобедренный треугольник ";
-	}
