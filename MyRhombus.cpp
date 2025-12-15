660d3df0fa0279ef73c6a5d3d8deb689d0dcb75a
Konstantin <Glefa86@mail.ru>
2025-12-15 20:24:04 +0300

    Delete MyRhombus.cpp

--- MyRhombus.cpp
+++
@@ -1,5 +0,0 @@
-﻿#include "MyRhombus.h"
-
-Rhombus::Rhombus(int side_a, int angle_A, int angle_B) : Parallelogramm(side_a, side_a, angle_A, angle_B) { // Вызов конструктора ромб
-		Name = "Hомб ";
-	}
