2c8f622131f1e8327d1b59a2f2ee70f8f3f60740
Konstantin <Glefa86@mail.ru>
2025-12-16 19:26:53 +0300

    Add files via upload

---
+++ DZ_PreprocessorDirectives.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+#define MODE 1 
+
+#ifdef MODE
+double add(double a, double b) {
+	return a + b;
+}
+int main() {
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	setlocale(LC_ALL, "RU");
+
+#if MODE == 0
+	std::cout << "Работаю в режиме тренеровки. \n";
+#elif MODE == 1
+	std::cout << "Работаю в боевом режиме \n";
+	std::cout << "Введите число 1: ";
+	double a;
+	std::cin >> a;
+	std::cout << "Введите число 2: ";
+	double b;
+	std::cin >> b;
+	std::cout << "Результат сложения: " << add(a, b);
+#else
+	std::cout << "Неизвестный режим. Завершение работы. \n";
+#endif
+	return 0;
+}
+
+#endif // MODE
+
+#ifndef MODE
+#error MODE НЕ ОПРЕДЕЛЕНО
+#endif
