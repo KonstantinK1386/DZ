742caf6bb4c3f4a8d7af7c1af34e54e6d868a01b
Konstantin <Glefa86@mail.ru>
2026-01-19 23:12:58 +0300

    Add files via upload

---
+++ My_first_cmake_prodject/My_first_cmake_prodject.cpp
@@ -0,0 +1,21 @@
+﻿// My_first_cmake_prodject.cpp: определяет точку входа для приложения.
+//
+
+#include <iostream>
+#include <Windows.h>
+
+
+int main(){
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	setlocale(LC_ALL, "RU");
+
+	std::string Name;
+	std::cout << "Введите пожалуйста своё имя: ";
+	std::cin >> Name;
+	std::cout << "Здравствуйте, " << Name <<'\n';
+	system("pause");
+
+
+	return 0;
+}
